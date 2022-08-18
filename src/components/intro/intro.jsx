cb270374d37c14d4bdef9eb90617cef05b073b29
Will Nascimento <69582935+will-nascimento@users.noreply.github.com>
2022-08-18 20:57:01 -0300

    chore: adjust intro

--- src/components/intro/intro.jsx
+++ src/components/intro/intro.jsx
@@ -2,6 +2,22 @@ import './intro.scss'
 
 export default function intro() {
   return (
-    <div className='intro' id='intro'>intro</div>
+    <div className='intro' id='intro'>
+      <div className="left">
+        <div className="imgContainer">
+          <img src="assets/eu quadrado.png" alt="" />
+        </div>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi there, I'm</h2>
+          <h1>Will Nascimento</h1>
+          <h3>React Developer <span></span></h3>
+        </div>
+        <a href="#portfolio" className="portfolio">
+          <img src="assets/down.png" alt="" />
+        </a>
+      </div>
+    </div>
   )
 }
