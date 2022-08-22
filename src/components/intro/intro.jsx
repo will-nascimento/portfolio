aa059a8d28b9bf5e992b35a3a3972259555f8e38
Will Nascimento <69582935+will-nascimento@users.noreply.github.com>
2022-08-21 23:22:07 -0300

    chore: add hooks

--- src/components/intro/intro.jsx
+++ src/components/intro/intro.jsx
@@ -1,6 +1,17 @@
 import './intro.scss'
+import { init } from 'ityped'
+import { useEffect, useRef } from 'react'
 
 export default function intro() {
+  const textRef = useRef();
+
+  useEffect(() => {
+    init(textRef.current, {
+      showCursor: false,
+      strings: ["ReactJs Developer", "ReactJs Developer"],
+    });
+  }, []);
+
   return (
     <div className='intro' id='intro'>
       <div className="left">
@@ -12,7 +23,7 @@ export default function intro() {
         <div className="wrapper">
           <h2>Hi there, I'm</h2>
           <h1>Will Nascimento</h1>
-          <h3>React  <span>Developer</span></h3>
+          <h3>  <span ref={textRef}></span></h3>
         </div>
         <a href="#portfolio" className="portfolio">
           <img src="assets/down.png" alt="" />
