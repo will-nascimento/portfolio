5beb09b1b0b0ff379f5583bc3d38fd12d50b279a
Will Nascimento <willnascimentowns@gmail.com>
2022-10-01 01:29:54 -0300

    chore: portfolio images

--- src/components/portfolio/portfolio.jsx
+++ src/components/portfolio/portfolio.jsx
@@ -5,14 +5,51 @@ export default function portfolio() {
     <div className='portfolio' id='portfolio'>
       <h1>Portfolio</h1>    
       <ul>
-        <li>Soon</li>
+        <li className='active'>Soon</li>
         <li>Soon</li>
         <li>Soon</li>
         <li>Soon</li>
         <li>Soon</li>
         <li>Soon</li>
       </ul> 
-      div.container
+      <div className="container">
+        <div className="item">
+          <img style={{width:"150px"}} src="https://static4.depositphotos.com/1009397/284/i/950/depositphotos_2846910-stock-photo-baby-cat-portrait.jpg" alt="" />
+          <h3>
+            Cat App - Soon
+          </h3>
+        </div>
+        <div className="item">
+          <img style={{width:"150px"}} src="https://static4.depositphotos.com/1009397/284/i/950/depositphotos_2846910-stock-photo-baby-cat-portrait.jpg" alt="" />
+          <h3>
+            Cat App - Soon
+          </h3>
+        </div>
+        <div className="item">
+          <img style={{width:"150px"}} src="https://static4.depositphotos.com/1009397/284/i/950/depositphotos_2846910-stock-photo-baby-cat-portrait.jpg" alt="" />
+          <h3>
+            Cat App - Soon
+          </h3>
+        </div>
+        <div className="item">
+          <img style={{width:"150px"}} src="https://static4.depositphotos.com/1009397/284/i/950/depositphotos_2846910-stock-photo-baby-cat-portrait.jpg" alt="" />
+          <h3>
+            Cat App - Soon
+          </h3>
+        </div>
+        <div className="item">
+          <img style={{width:"150px"}} src="https://static4.depositphotos.com/1009397/284/i/950/depositphotos_2846910-stock-photo-baby-cat-portrait.jpg" alt="" />
+          <h3>
+            Cat App - Soon
+          </h3>
+        </div>
+        <div className="item">
+          <img style={{width:"150px"}} src="https://static4.depositphotos.com/1009397/284/i/950/depositphotos_2846910-stock-photo-baby-cat-portrait.jpg" alt="" />
+          <h3>
+            Cat App - Soon
+          </h3>
+        </div>
+      </div>
     </div>
   )
 }
