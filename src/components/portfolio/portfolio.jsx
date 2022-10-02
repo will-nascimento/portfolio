1a353409bcbb52a80d8e11487f78bfb9a1e1a2de
Will Nascimento <willnascimentowns@gmail.com>
2022-10-02 20:15:30 -0300

    chore: portfolioList improvements

--- src/components/portfolio/portfolio.jsx
+++ src/components/portfolio/portfolio.jsx
@@ -1,7 +1,12 @@
 import PortfolioList from '../portfolioList/portfolioList';
 import './portfolio.scss'
+import { useState, useEffect } from 'react';
+import { featuredPortfolio, webPortfolio, mobilePortfolio, designPortfolio, contentPortfolio } from '../../data';
 
 export default function Portfolio() {
+
+  const [selected, setSelected] = useState("featured")
+  const [data, setData] = useState([])
 
   const list = [
     {
@@ -9,69 +14,65 @@ export default function Portfolio() {
       title: "Featured",
     },
     {
-      id: "featured",
+      id: "web",
       title: "Featured",
     },
     {
-      id: "featured",
+      id: "mobile",
       title: "Featured",
     },
     {
-      id: "featured",
+      id: "design",
       title: "Featured",
     },
     {
-      id: "featured",
+      id: "content",
       title: "Featured",
     },
   ];
 
+  useEffect(() => {
+    switch (selected) {
+      case "featured": setData(featuredPortfolio);
+        break;
+      case "web": setData(webPortfolio);
+        break;
+      case "mobile": setData(mobilePortfolio);
+        break;
+      case "design": setData(designPortfolio);
+        break;
+      case "content": setData(contentPortfolio);
+        break;
+      default: setData(featuredPortfolio);
+        break;
+    }
+  }, [selected]);
+
   return (
     <div className='portfolio' id='portfolio'>
-      <h1>Portfolio</h1>    
+      <h1>Portfolio</h1>
       <ul>
         {list.map((item) => (
-          <PortfolioList title={item.title}/>
+          <PortfolioList
+            title={item.title}
+            active={selected === item.id}
+            setSelected={setSelected}
+            id={item.id}
+          />
         ))}
-      </ul> 
+      </ul>
       <div className="container">
-        <div className="item">
-        <img src="https://static4.depositphotos.com/1009397/284/i/950/depositphotos_2846910-stock-photo-baby-cat-portrait.jpg" alt="" />
-          <h3>
-            Cat App - Soon
-          </h3>
-        </div>
-        <div className="item">
-        <img src="https://static4.depositphotos.com/1009397/284/i/950/depositphotos_2846910-stock-photo-baby-cat-portrait.jpg" alt="" />
-          <h3>
-            Cat App - Soon
-          </h3>
-        </div>
-        <div className="item">
-        <img src="https://static4.depositphotos.com/1009397/284/i/950/depositphotos_2846910-stock-photo-baby-cat-portrait.jpg" alt="" />
-          <h3>
-            Cat App - Soon
-          </h3>
-        </div>
-        <div className="item">
-        <img src="https://static4.depositphotos.com/1009397/284/i/950/depositphotos_2846910-stock-photo-baby-cat-portrait.jpg" alt="" />
-          <h3>
-            Cat App - Soon
-          </h3>
-        </div>
-        <div className="item">
-        <img src="https://static4.depositphotos.com/1009397/284/i/950/depositphotos_2846910-stock-photo-baby-cat-portrait.jpg" alt="" />
-          <h3>
-            Cat App - Soon
-          </h3>
-        </div>
-        <div className="item">
-          <img src="https://static4.depositphotos.com/1009397/284/i/950/depositphotos_2846910-stock-photo-baby-cat-portrait.jpg" alt="" />
-          <h3>
-            Cat App - Soon
-          </h3>
-        </div>
+        {data.map((d) => (
+          <div className="item">
+            <img
+              src={d.img}
+         alt="" />
+            <h3>
+              {d.title}
+            </h3>
+          </div>
+        ))}
       </div>
     </div>
-  )
+  );
 }
