8e35963c1dabd462c01c5dd610d891611f3a3512
Will Nascimento <willnascimentowns@gmail.com>
2022-10-16 20:29:47 -0300

    chore: testimonials

--- src/components/testimonials/testimonials.jsx
+++ src/components/testimonials/testimonials.jsx
@@ -2,6 +2,20 @@ import './testimonials.scss';
 
 export default function testimonials() {
   return <div className='testimonials' id='testimonials'>
-      testimonials
+      <h1>Testimonials</h1>
+      <div className="container">
+        <div className="card">
+          <div className="top">
+            <img src="assets/right arrow.png" alt="" className="left" />
+            <img src="https://images.unsplash.com/photo-1559703248-dcaaec9fab78?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80" alt="" className="user" />
+            <img src="assets/youtube.png" alt="" className="right" />
+          </div>
+          <div className="center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas perspiciatis minima adipisci.</div>
+          <div className="bottom">
+            <h3>Lucelita</h3>
+            <h4>CEO of MEGGA</h4>
+          </div>
+        </div>
+      </div>
       </div>;
 }
--- src/components/works/works.jsx
+++ src/components/works/works.jsx
@@ -28,8 +28,9 @@ export default function Works() {
     ];
 
   const handleClick = (way)=>{
-    way === "left" ? setCurrentSlide(currentSlide > 0 ? currentSlide-1 : 2) :
-                     setCurrentSlide(currentSlide <data.length -1 ? currentSlide +1 : 0); 
+    way === "left" 
+    ? setCurrentSlide(currentSlide > 0 ? currentSlide-1 : 2) 
+    : setCurrentSlide(currentSlide <data.length -1 ? currentSlide +1 : 0); 
   }
   return (
     <div className='works' id='works'>
